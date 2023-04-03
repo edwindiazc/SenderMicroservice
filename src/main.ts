95e87d0f98bbefa29e73af465ee30f4ac18ab998
edwindiazc <edwin@group-tgs.com>
2023-04-03 15:17:35 -0600

    variables for pusher added

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,54 @@
 import { NestFactory } from '@nestjs/core';
-import {  ValidationError, ValidationPipe, } from '@nestjs/common';
-import { Transport,RpcException } from '@nestjs/microservices';
+import { ValidationError, ValidationPipe, } from '@nestjs/common';
+import { Transport, RpcException } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://localhost:5672'],
-      queue: 'sender_queue',
-      queueOptions: {
-        durable: false
+
+  const app = await NestFactory.create(AppModule);
+
+  await app.connectMicroservice(
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'sender_queue',
+        queueOptions: {
+          durable: false
+        },
       },
-    },
-  });
+    }
+  );
+
+  await app.connectMicroservice(
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'notifier_queue',
+        queueOptions: {
+          durable: false
+        },
+      },
+    }
+  );
+
+  await app.connectMicroservice(
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'caller_queue',
+        queueOptions: {
+          durable: false
+        },
+      },
+    }
+  );
+
+  await app.startAllMicroservices();
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -27,6 +61,6 @@ async function bootstrap() {
       },
     })
   );
-  await app.listen();
+  await app.listen(3001);
 }
 bootstrap();
