fda0e0769d6f4c16c36dbfcb2cda5772adea2b30
edwindiazc <edwin@group-tgs.com>
2023-04-03 09:16:11 -0600

    nest and rabbitmq configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
+import {ClientsModule, Transport} from '@nestjs/microservices';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'EMAIL_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'sender_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'sender_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+      {
+        name: 'TWILLO_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'sender_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
